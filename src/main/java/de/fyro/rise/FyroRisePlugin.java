440505f86f1db8b09a0235c8d61f07946b01ef01
manuelreimerprivat-beep <manuel.reimer.privat@gmail.com>
2026-09-14 21:20:39 +0200

    feat: wire the player display service into FYRO RISE

--- src/main/java/de/fyro/rise/FyroRisePlugin.java
+++ src/main/java/de/fyro/rise/FyroRisePlugin.java
@@ -1,6 +1,7 @@
 package de.fyro.rise;
 
 import de.fyro.rise.command.RiseCommand;
+import de.fyro.rise.display.DisplayService;
 import de.fyro.rise.dungeon.DungeonService;
 import de.fyro.rise.game.GameService;
 import de.fyro.rise.gear.GearService;
@@ -18,6 +19,7 @@ import java.util.Objects;
 public final class FyroRisePlugin extends JavaPlugin {
     private DataStore data;
     private GearService gear;
+    private DisplayService display;
     private GameService game;
     private QuestService quests;
     private SocialService social;
@@ -30,9 +32,10 @@ public final class FyroRisePlugin extends JavaPlugin {
 
         data = new DataStore(this);
         gear = new GearService(this);
-        game = new GameService(this, data, gear);
+        display = new DisplayService(this, data);
+        game = new GameService(this, data, gear, display);
         quests = new QuestService(this, data, game);
-        social = new SocialService(this, data, game);
+        social = new SocialService(this, data, game, display);
         dungeons = new DungeonService(this, game, gear, social, quests);
 
         RiseCommand commandHandler = new RiseCommand(this, data, game, gear, quests, social, dungeons);
@@ -41,7 +44,7 @@ public final class FyroRisePlugin extends JavaPlugin {
         riseCommand.setTabCompleter(commandHandler);
 
         Bukkit.getPluginManager().registerEvents(
-                new GameListener(this, data, game, gear, quests, dungeons), this);
+                new GameListener(this, data, game, gear, quests, dungeons, display), this);
 
         long minutes = Math.max(1, getConfig().getLong("server.autosave-minutes", 5));
         Bukkit.getScheduler().runTaskTimer(this, () -> {
@@ -57,6 +60,7 @@ public final class FyroRisePlugin extends JavaPlugin {
         if (dungeons != null) dungeons.shutdown();
         if (data != null) data.saveAll();
         if (social != null) social.saveGuilds();
+        if (display != null) display.shutdown();
         getLogger().info("FYRO-RISE sicher deaktiviert.");
     }
 
